e3f40b5d7e6b107b844e5eaf7414fd68c18a5cdd
maximuse.domn <user.maximuse@gmail.com>
2025-01-31 17:13:30 +0200

    Created project folders structure, base classes(Command, ViewModel); created general view of main window: main menu and tabs

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using CryptoInfo.Helpers.Commands;
+
+namespace CryptoInfo.ViewModels
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        private string _CurrentTheme = "Light";
+
+        private string _CurrentLanguage = "en";
+
+        public ICommand ToggleThemeCommand { get; set; }
+        public ICommand ChangeLanguageCommand { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+            ToggleThemeCommand = new RelayCommand(ToggleTheme, CanToggleTheme);
+            ChangeLanguageCommand = new RelayCommand(ChangeLanguage, CanChangeLanguage);
+        }
+
+        private void ToggleTheme(object? theme)
+        {
+            _CurrentTheme = theme as string;
+
+        }
+
+        private bool CanToggleTheme(object? theme)
+        {
+            if(_CurrentTheme == (string?)theme) return false;
+            return true;
+        }
+
+        private void ChangeLanguage(object? languageCode)
+        {
+            _CurrentLanguage = languageCode as string;
+
+        }
+
+        private bool CanChangeLanguage(object? languageCode)
+        {
+            if (_CurrentLanguage == (string?)languageCode) return false;
+            return true;
+        }
+
+    }
+}
